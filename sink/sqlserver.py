45f1562890c26074918de6a82112fa538baa55ff
David Liu <david-khala@hotmail.com>
2024-11-10 21:50:14 +0800

    backup

--- sink/sqlserver.py
+++ sink/sqlserver.py
@@ -1,4 +1,7 @@
-dbutils.widgets.text("table", "")
+
+dbutils.widgets.text("source_table", "`azure-open-datasets`.nyctlc.yellow")
+dbutils.widgets.text("target_table", "yellow")
+
 # Choose "append" to add rows to the table or "overwrite" to replace the table
 dbutils.widgets.dropdown("mode","append", ["append", "overwrite"] )
 
@@ -6,12 +9,12 @@ server = dbutils.secrets.get(scope = "sqlserver", key = "server")
 database = dbutils.secrets.get(scope = "sqlserver", key = "database")
 
 
-
+source_df = spark.read.table(dbutils.widgets.get("source_table"))
 
 # Write the DataFrame to SQL Server using JDBC
 source_df.write.jdbc(
     url=f"jdbc:sqlserver://{server};databaseName={database}",
-    table=dbutils.widgets.get("table"),
+    table=dbutils.widgets.get("target_table"),
     mode=dbutils.widgets.get("mode"),  
     properties={
         "user": dbutils.secrets.get(scope = "sqlserver", key = "user"),
@@ -19,6 +22,5 @@ source_df.write.jdbc(
         "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
     }
 )
-
-
-
+# also accept number type
+dbutils.notebook.exit(source_df.count())
