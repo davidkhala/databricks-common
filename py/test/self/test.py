ea011fb85b16694c51351fd7d7c3ddd932ee93d0
David Liu <david-khala@hotmail.com>
2024-12-06 17:41:55 +0800

    simplify test

--- py/test/self/test.py
+++ py/test/self/test.py
@@ -3,7 +3,6 @@ import unittest
 class TestCase(unittest.TestCase):
     def test_ping(self):
         from davidkhala.databricks.connect import DatabricksConnect
-        from da.common.local import
         DatabricksConnect.ping(True)
 
 
