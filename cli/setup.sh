dfbfc226c0b29500adc9f54245b5b46ad079a90e
David Liu <david-khala@hotmail.com>
2024-12-15 00:32:06 +0800

    fixing ci

--- cli/setup.sh
+++ cli/setup.sh
@@ -3,11 +3,11 @@ install() {
   curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sudo sh
 }
 login-serverless() {
-  local url=$1                                   # workspace url. in format of `adb-662901427557763.3.azuredatabricks.net`
+  local workspace=$1                                   # workspace instance name. in format of `adb-662901427557763.3.azuredatabricks.net`
   local pat=$2                                   # Personal access token
   export DATABRICKS_SERVERLESS_COMPUTE_ID='auto' # not harm for profile file
   if [ -n "$2" ]; then
-    databricks configure --token --host "https://$url" <<<"$pat"
+    databricks configure --token --host "https://$workspace" <<<"$pat"
   else
     databricks configure
   fi
@@ -15,11 +15,11 @@ login-serverless() {
 
 }
 login() {
-  local url=$1 # workspace url. in format of `adb-662901427557763.3.azuredatabricks.net`
+  local workspace=$1 # workspace instance name. in format of `adb-662901427557763.3.azuredatabricks.net`
   local pat=$2 # Personal access token
   if [ -n "$3" ]; then
     export DATABRICKS_CLUSTER_ID=$3
-    databricks configure --token --host https://$url <<<$pat
+    databricks configure --token --host "https://$workspace" <<<"$pat"
   else
     # interactive auto cluster_id discovery
     databricks configure --configure-cluster
